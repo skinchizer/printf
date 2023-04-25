a155e7afc4e89514a9b8f33954712135f0558365
skinchizer <mezo214@gmail.com>
2023-04-25 04:29:17 -0700

    s

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int len;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    printf("Length:[%d, %i]\n", len, len);
+    return (0);
+}
